2fd953058fa0e9e0f40c898f371445c585778cac
nesacar <nebojsart1409@yahoo.com>
2018-03-12 15:24:35 +0100

    index

--- resources/views/themes/cim/index.blade.php
+++ resources/views/themes/cim/index.blade.php
@@ -15,6 +15,7 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.carousel.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&amp;subset=latin-ext" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
     {!! HTML::style($theme->slug.'/css/style.css') !!}
