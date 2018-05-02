cfa2221918549da8d5167a812aac07a33dd07c2f
nesacar <nebojsart1409@yahoo.com>
2018-05-02 12:06:19 +0200

    posts link fix

--- resources/views/themes/cim/partials/owl-carousel.blade.php
+++ resources/views/themes/cim/partials/owl-carousel.blade.php
@@ -1,15 +1,16 @@
 @if(count($posts)>0)
 <div class="owl-carousel owl-theme">
     @foreach($posts as $post)
+        @php $link = \App\Post::getPostLink($post); @endphp
         <div class="item col-md-12">
             <div class=img-wrapper>
-                <a href=#>
+                <a href="{{ $link }}">
                     {!! HTML::Image($post->image, $post->title, array('class' => 'img-fluid')) !!}
                 </a>
             </div>
             <div class=text-holder>
                 <h5>
-                    <a href=#>{{ $post->title }}</a>
+                    <a href="{{ $link }}">{{ $post->title }}</a>
                 </h5>
                 <p class=date>
                     <span>{{ \Carbon\Carbon::parse($post->publish_at)->format('d M Y') }}</span>
